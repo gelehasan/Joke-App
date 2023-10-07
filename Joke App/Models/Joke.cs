a4a0af046a766235b0e16dd977338428781eb62c
Gele <gellehussen@gmail.com>
2023-10-07 22:39:10 +0200

    connected the application to mysql server database

--- Joke App/Models/Joke.cs
+++ Joke App/Models/Joke.cs
@@ -3,6 +3,11 @@ namespace Joke_App.Models
 {
 	public class Joke
 	{
+
+		public int Id { get; set; }
+		public string JokeQuestion { get; set; }
+		public string JokeAnswer { get; set; }
+
 		public Joke()
 		{
 		}
--- Joke App/Program.cs
+++ Joke App/Program.cs
@@ -6,8 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//   options.UseSqlite(connectionString));
+
+builder.Services.AddDbContextPool<ApplicationDbContext>(opt =>
+{
+    opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+});
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
